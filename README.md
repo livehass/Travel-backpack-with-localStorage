30e29b4c335acdc0693d3db7b72cf86537dba066
livehass <silva.felipe12@hotmail.com>
2023-05-08 20:22:44 -0300

    final commits

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Travel-backpack-with-localStorage
-A simple travel backpack, with itens list and storage in localStorage.
+
+## A simple travel backpack, with itens list and storage in localStorage.
+
+### just experiencing some things that i'm lerging at the moment.
+
+
+[live demo here]()
--- js/import-itens.js
+++ js/import-itens.js
@@ -1,1 +1,16 @@
-const send = document.getElementById(".send2")
+//i will come back for these later, for now i dont have the knowledge, to make this work
+
+const send = document.querySelector(".send2");
+
+send.addEventListener("click", (e)=>{
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", "https://gist.githubusercontent.com/livehass/5c1e00347a8961031a193cbff7e504b9/raw/a47a257a4af216066822b9d87356205b4ba48f6e/my-list.json")
+
+    xhr.addEventListener("load", (e)=>{
+        
+        
+       createElement(e, e);
+    })
+    xhr.send();
+    console.log()
+})
