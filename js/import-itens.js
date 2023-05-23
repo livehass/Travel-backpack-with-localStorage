fee37890ce2a9911c7fbf43fd2b0e2d3babf37b5
Felipe <silva.felipe12@hotmail.com>
2023-05-23 17:59:51 -0300

    Update import-itens.js

--- js/import-itens.js
+++ js/import-itens.js
@@ -1,18 +1,14 @@
-/*i will come back for these later, for now i dont have the knowledge, to make this work
+/*
 send.addEventListener("click", (e)=>{
     var xhr = new XMLHttpRequest();
     xhr.open("get", "https://gist.githubusercontent.com/livehass/5c1e00347a8961031a193cbff7e504b9/raw/a47a257a4af216066822b9d87356205b4ba48f6e/my-list.json")
 
-    xhr.addEventListener("load", (e)=>{
-        
-        
+    xhr.addEventListener("load", (e)=>  
        createElement(e, e);
     })
-    xhr.send();
-    
+    xhr.send(); 
 })
 */
-//the project its complete but
 import data from '../myitens.json' assert {type: "json"};
 const send = document.querySelector(".send2");
 send.addEventListener("click", (e)=>{
